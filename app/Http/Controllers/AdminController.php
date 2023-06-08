830bb06aa7a2c522359721e88f672d27e1746653
MA-Moran <118410881+MA-Moran@users.noreply.github.com>
2023-06-08 12:24:08 -0600

    Add files via upload

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Admin;
+
+class AdminController extends Controller
+{
+    // Log In
+    function login(){
+        return view('login');
+    }
+
+    // Check Login
+    function check_login(Request $request){
+        $request->validate([
+            'username'=>'required',
+            'password'=>'required',  
+        ]);
+
+        $admin=Admin::where(['username'=>$request->username,'password'=>sha1($request->password)])->count(); 
+        
+        if($admin>0){
+            $adminData=Admin::where(['username'=>$request->username,'password'=>sha1($request->password)])->get();
+            session(['adminData'=>$adminData]);
+            return redirect('admin');
+        }else{
+            return redirect('admin/login')->with('msg','Invalid username/Password!!');
+        }
+    }
+    // Log Out
+    function logout(){
+        session()->forget(['adminData']);
+        return redirect('admin/login');
+    }
+
+}
---
+++ storage/framework/views/20208855e1c6e579b0b53880d82d18c6d8233c7f.php
@@ -0,0 +1,40 @@
+
+<?php $__env->startSection('content'); ?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+    <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">Add Room Type
+        <a href="<?php echo e(url('admin/roomtype')); ?>" class="float-right btn btn-success btn sm">View All</a>
+        </h6>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-bordered" >
+            <tr>
+            <tr>
+                <th>Title</th>
+                <td><?php echo e($data->title); ?></td>
+                </tr>
+                <th>Price  $  </th>
+                <td><?php echo e($data->price); ?></td>
+                </tr>
+                <tr>
+                <th>Detail</th>
+                <td><?php echo e($data->detail); ?></td>
+                </tr>
+            </table>
+            </form>
+        </div>
+    </div>
+</div>
+
+</div>
+<!-- /.container-fluid -->
+   
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\hotelmanagement\resources\views/roomtype/show.blade.php ENDPATH**/ ?>
